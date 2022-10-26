b94803c496438ff2294f9dab96744964188d8c96
George Campbell <geocolumbus@gmail.com>
2022-10-25 21:09:01 -0400

    Update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Monday, October 24th - still adjustable.</h1>"
+    return "<h1 style='color:red'>Tuesday, October 25th</h1>"
 
 
 if __name__ == "__main__":
